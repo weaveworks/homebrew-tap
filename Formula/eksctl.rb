d5752e0130bdec87158196f4d41b59769543d161
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-07-05 09:18:01 +0100

    Add dependency on vendored aws-iam-authenticator

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -6,6 +6,7 @@ class Eksctl < Formula
   sha256 "80b2613dc3ec8284c5b8f754fed7f01917585e92a541f8fb756549cb11dd904f"
   
   depends_on "kubernetes-cli"
+  depends_on "eksctl-aws-iam-authenticator"
 
   def install
     bin.install "eksctl"
