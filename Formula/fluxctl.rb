0282ae88fce334329e4d14b5da69366767eb9284
Daniel Holbach <daniel@weave.works>
2018-08-28 14:16:58 +0100

    simplify formula, stole from eksctl-aws-iam-authenticator.rb

--- Formula/fluxctl.rb
+++ Formula/fluxctl.rb
@@ -1,15 +1,14 @@
 class Fluxctl < Formula
   desc "fluxctl talks to Weave Flux and helps you deploy your code"
   homepage "https://github.com/weaveworks/flux"
-  url "https://github.com/weaveworks/flux/releases/download/1.5.0/fluxctl_darwin_amd64"
   version "1.5.0"
+  url "https://github.com/weaveworks/flux/releases/download/#{version}/fluxctl_darwin_amd64"
   sha256 "c93774f6111b1edb4e00e32749618ae5fdf07daa2370d0e54f2701121c540c9d"
 
-  bottle :unneeded
   depends_on "kubernetes-cli"
 
   def install
-    libexec.install Dir["*"].shift => "fluxctl"
-    bin.install_symlink libexec/"fluxctl"
+    bin.install "fluxctl_darwin_amd64"
+    mv bin/"fluxctl_darwin_amd64", bin/"fluxctl"
   end
 end
