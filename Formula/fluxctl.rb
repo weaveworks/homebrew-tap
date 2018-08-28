a84f67b39c15e89141722e05673031c245a2d29d
Daniel Holbach <daniel@weave.works>
2018-08-28 14:20:33 +0100

    don't depend on kubernetes-cli

--- Formula/fluxctl.rb
+++ Formula/fluxctl.rb
@@ -5,8 +5,6 @@ class Fluxctl < Formula
   url "https://github.com/weaveworks/flux/releases/download/#{version}/fluxctl_darwin_amd64"
   sha256 "c93774f6111b1edb4e00e32749618ae5fdf07daa2370d0e54f2701121c540c9d"
 
-  depends_on "kubernetes-cli"
-
   def install
     bin.install "fluxctl_darwin_amd64"
     mv bin/"fluxctl_darwin_amd64", bin/"fluxctl"
