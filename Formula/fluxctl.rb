a82a09c9e950490897a9a800c4ed5d4826ab3b78
Daniel Holbach <daniel@weave.works>
2018-09-27 12:13:03 +0200

    update to flux 1.7.1

--- Formula/fluxctl.rb
+++ Formula/fluxctl.rb
@@ -1,9 +1,9 @@
 class Fluxctl < Formula
   desc "fluxctl talks to Weave Flux and helps you deploy your code"
   homepage "https://github.com/weaveworks/flux"
-  version "1.7.0"
+  version "1.7.1"
   url "https://github.com/weaveworks/flux/releases/download/#{version}/fluxctl_darwin_amd64"
-  sha256 "1d2971c67a12fd24852ed541506283fe405f5569a43941febe4b9de23e2294b7"
+  sha256 "4e87986ea23f27313f0d2d42e9747ca92f7c51f262eec11ec8f5ecce9203ed04"
 
   def install
     bin.install "fluxctl_darwin_amd64"
