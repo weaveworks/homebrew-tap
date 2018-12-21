3345a2fdf2fd35898f21a990b5b9256fed1ec752
weaveworksbot <team+gitbot@weave.works>
2018-12-21 16:45:21 +0000

    Brew formula update for eksctl version 0.1.14

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -1,9 +1,9 @@
 class Eksctl < Formula
   desc "a CLI for Amazon EKS"
   homepage "https://eksctl.io/"
-  url "https://github.com/weaveworks/eksctl/releases/download/0.1.13/eksctl_Darwin_amd64.tar.gz"
-  version "0.1.13"
-  sha256 "c365e9591002aa1d0725ca7291bc1e870001e94b233cef0a820956280aff01fa"
+  url "https://github.com/weaveworks/eksctl/releases/download/0.1.14/eksctl_Darwin_amd64.tar.gz"
+  version "0.1.14"
+  sha256 "7ad3a5393b2afe919031e055734a89c380b9149183fd1d5bf78c83e36ba48564"
   
   depends_on "kubernetes-cli"
   depends_on "eksctl-aws-iam-authenticator"
