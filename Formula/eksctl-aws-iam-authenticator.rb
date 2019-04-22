304f208153a76d981115cc1792243e9f4684764a
Christopher Hein <me@chrishein.com>
2019-04-22 18:41:57 +0000

    adding 0.4.0 authenticator
    Signed-off-by: Christopher Hein <me@chrishein.com>

--- Formula/eksctl-aws-iam-authenticator.rb
+++ Formula/eksctl-aws-iam-authenticator.rb
@@ -1,12 +1,12 @@
 class EksctlAwsIamAuthenticator < Formula
   desc "A tool to use AWS IAM credentials to authenticate to a Kubernetes cluster"
   homepage "https://github.com/kubernetes-sigs/aws-iam-authenticator"
-  version "0.3.0"
-  url "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v#{version}/heptio-authenticator-aws_#{version}_darwin_amd64"
-  sha256 "9585d9af8d2f3b46b31649fb549978f4aedf2d62113fd42b57f9903ad71811c2"
-  
+  version "0.4.0"
+  url "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v#{version}/aws-iam-authenticator_#{version}_darwin_amd64"
+  sha256 "b049e08b46d36d6d5087709e5956d6be8f70fc221738d3dd11488e87ce17387f"
+
   def install
-    bin.install "heptio-authenticator-aws_#{version}_darwin_amd64"
-    mv bin/"heptio-authenticator-aws_#{version}_darwin_amd64", bin/"heptio-authenticator-aws"
+    bin.install "aws-iam-authenticator_#{version}_darwin_amd64"
+    mv bin/"aws-iam-authenticator_#{version}_darwin_amd64", bin/"aws-iam-authenticator"
   end
 end
