cb22a93a24e28a8c425a5543a56abfa2737553be
Ilya Dmitrichenko <errordeveloper@gmail.com>
2019-04-30 12:27:28 +0100

    Revert "Brew formula update for eksctl version 0.1.30"
    This reverts commit 327edfdccc79c7c2f296a3c6f55b5e513365f50e.

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -1,9 +1,9 @@
 class Eksctl < Formula
   desc "a CLI for Amazon EKS"
   homepage "https://eksctl.io/"
-  url "https://github.com/weaveworks/eksctl/releases/download/0.1.30/eksctl_Darwin_amd64.tar.gz"
-  version "0.1.30"
-  sha256 "8ec47280f9d522c510429dcc5b665c2ad2eaeb0578a96f8ba4a504f1e76bd887"
+  url "https://github.com/weaveworks/eksctl/releases/download/0.1.29/eksctl_Darwin_amd64.tar.gz"
+  version "0.1.29"
+  sha256 "6ea253e76ee1b8e863c421204048740f3a8dbf7d047aa536f51e6af1f2cf7669"
   
   depends_on "kubernetes-cli"
   depends_on "aws-iam-authenticator"
