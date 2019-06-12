848a0d89fe022edc63aa792bc5479686a268abeb
weaveworksbot <team+gitbot@weave.works>
2019-06-12 16:00:17 +0100

    Brew formula update for footloose version 0.5.0

--- Formula/footloose.rb
+++ Formula/footloose.rb
@@ -2,9 +2,9 @@
 class Footloose < Formula
   desc "Containers that look like Virtual Machines"
   homepage "https://github.com/weaveworks/footloose"
-  url "https://github.com/weaveworks/footloose/releases/download/0.4.0/footloose-0.4.0-darwin-x86_64.tar.gz"
-  version "0.4.0"
-  sha256 "2934ff6012bb90c906571504a40a84a6e649a65a9913dfb5e8c49899b693f34b"
+  url "https://github.com/weaveworks/footloose/releases/download/0.5.0/footloose-0.5.0-darwin-x86_64.tar.gz"
+  version "0.5.0"
+  sha256 "0b989621b8141dd31eb90fe0bb3aa5a12147a807dd86e7d33c990ec69e380550"
 
   def install
     bin.install "footloose"
