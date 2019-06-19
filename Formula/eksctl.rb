aa5c904b89dcc5dba218b326f11391b7c85067fe
Ilya Dmitrichenko <errordeveloper@gmail.com>
2019-06-19 21:26:36 +0100

    Fix version

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,7 +2,7 @@ class Eksctl < Formula
   desc "a CLI for Amazon EKS"
   homepage "https://eksctl.io/"
   url "https://github.com/weaveworks/eksctl/releases/download/0.1.37/eksctl_Darwin_amd64.tar.gz"
-  version "latest_release"
+  version "0.1.37"
   sha256 "b457e5b6e4fdafabf0c658606036b7ad152844e4ddfb0c5d1555f8f004fb5093"
   
   depends_on "kubernetes-cli"
