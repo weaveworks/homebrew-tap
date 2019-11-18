c45f24f4ec0051a1a2558012574ffffb921428b9
cpu1 <patwal.chetan@gmail.com>
2019-11-19 00:40:51 +0530

    Brew formula update for eksctl version 0.10.0

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,14 +2,14 @@
 class Eksctl < Formula
   desc "The official CLI for Amazon EKS"
   homepage "https://eksctl.io/"
-  version "0.9.0"
+  version "0.10.0"
 
   if OS.mac?
-    url "https://github.com/weaveworks/eksctl/releases/download/0.9.0/eksctl_Darwin_amd64.tar.gz"
-    sha256 "70d45ab2196cdd89ce24c23621e053af0256a775ca8a8d96ae864f04dc31201c"
+    url "https://github.com/weaveworks/eksctl/releases/download/0.10.0/eksctl_Darwin_amd64.tar.gz"
+    sha256 "3780c5d4b17f9e44ea703acd24a3be37eeffd6fce5b8277fcdd9489126402f10"
   elsif OS.linux?
-    url "https://github.com/weaveworks/eksctl/releases/download/0.9.0/eksctl_Linux_amd64.tar.gz"
-    sha256 "2c2f55a7df99b986d5431a287a3ba198efab71271e2b80989bfc5d926ce4ae17"
+    url "https://github.com/weaveworks/eksctl/releases/download/0.10.0/eksctl_Linux_amd64.tar.gz"
+    sha256 "f48e56ebbfe4648142c983b61df1bf577807e5de61126a1ee5e264b3eda8a71a"
   end
   
   depends_on "kubernetes-cli"
