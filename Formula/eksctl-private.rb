7447b89b606329fbadb4edc854d599bc9759fbaa
Martina Iglesias <martina-if@users.noreply.github.com>
2020-04-21 15:47:40 +0200

    Revert to eksct-private 0.13.0

--- Formula/eksctl-private.rb
+++ Formula/eksctl-private.rb
@@ -5,14 +5,14 @@ class EksctlPrivate < Formula
   version "0.25.0"
 
   if OS.mac?
-    url "https://github.com/weaveworks/eksctl-private/releases/download/0.25.0/eksctl_Darwin_amd64.tar.gz"
-    sha256 "ec53d53940dc44012d4cf666740c593288377345c31918fde1922333cc6465d1"
+    url "https://github.com/weaveworks/eksctl-private/releases/download/0.13.0/eksctl_Darwin_amd64.tar.gz"
+    sha256 "a94a0c78a939cd7c9b6a5e0b22d95799ca284bf7339ef11ce17be47ba6dd7a56"
   elsif OS.linux?
-    url "https://github.com/weaveworks/eksctl-private/releases/download/0.25.0/eksctl_Linux_amd64.tar.gz"
-    sha256 "0def9d793773d6dd5599057c0e4d96c6b15f305fd4fe7115eb907b9da1a4526e"
+    url "https://github.com/weaveworks/eksctl-private/releases/download/0.13.0/eksctl_Linux_amd64.tar.gz"
+    sha256 "edc1a143b44cb360af585da4e1c15e472ba9636a8166197ba40cf446d6dcb34f"
   end
   
-  head https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_Darwin_amd64.tar.gz
+  head "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_Darwin_amd64.tar.gz"
   
   depends_on "kubernetes-cli"
   depends_on "aws-iam-authenticator"
