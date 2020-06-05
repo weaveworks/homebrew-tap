5643b95f1038a6990f9b3cc586fef518b6893568
weaveworksbot <team+gitbot@weave.works>
2020-06-05 18:05:07 +0100

    Brew formula update for eksctl version 0.21.0

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,14 +2,17 @@
 class Eksctl < Formula
   desc "The official CLI for Amazon EKS"
   homepage "https://eksctl.io/"
-  version "0.20.0"
+  version "0.21.0"
+  bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/weaveworks/eksctl/releases/download/0.20.0/eksctl_Darwin_amd64.tar.gz"
-    sha256 "a96d4b367522461394e7546578bae9113bdd9703e7c8144b5b815a9d329a193a"
+    url "https://github.com/weaveworks/eksctl/releases/download/0.21.0/eksctl_Darwin_amd64.tar.gz"
+    sha256 "3cdcbb1792bb131cc0ed944cbfc51dd6f1b2261a480436efc6f8124dea7c8c14"
   elsif OS.linux?
-    url "https://github.com/weaveworks/eksctl/releases/download/0.20.0/eksctl_Linux_amd64.tar.gz"
-    sha256 "069f1980a7942a038e181e7e75d23fa3d8102c2667c1560475cf3bfb570c745a"
+    if Hardware::CPU.intel?
+      url "https://github.com/weaveworks/eksctl/releases/download/0.21.0/eksctl_Linux_amd64.tar.gz"
+      sha256 "4573bca35af67fa002fb722b4d41fae2224a27576619ed2f1e269dd7bd15c214"
+    end
   end
   
   head "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_Darwin_amd64.tar.gz"
@@ -19,5 +22,13 @@ class Eksctl < Formula
 
   def install
     bin.install "eksctl"
+    
+    # Install bash completion
+    output = Utils.popen_read("#{bin}/eksctl completion bash")
+    (bash_completion/"eksctl").write output
+    
+    # Install zsh completion
+    output = Utils.popen_read("#{bin}/eksctl completion zsh")
+    (zsh_completion/"_eksctl").write output
   end
 end
