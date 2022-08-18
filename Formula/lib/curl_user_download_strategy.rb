4cdc3461f4c78582ea11d2b06b8acc6fc8019993
Simon Howe <simon@weave.works>
2022-08-18 08:17:34 +0200

    Update env vars used

--- Formula/lib/curl_user_download_strategy.rb
+++
@@ -1,8 +0,0 @@
-class CurlUserDownloadStrategy < CurlDownloadStrategy
-  def initialize(url, name, version, **meta)
-    meta ||= {}
-    meta[:user] ||= ENV['HOMEBREW_CURL_USER']
-    super(url, name, version, meta)
-  end
-end
-
